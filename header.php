2a8fc0ce9174da63b9b065847cd9eed56789a482
manishsuwal <enwil410@gmail.com>
2013-10-25 18:16:07 +0545

    Added Custom Header Code to Display Custom Header

--- header.php
+++ header.php
@@ -30,6 +30,9 @@
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
+		<!-- Custom Header Code -->
+		<div align="center" id="header-image"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></div>
+		<!-- .Custom Header Code -->
 
 		<nav role="navigation" class="site-navigation main-navigation">
 			<h1 class="assistive-text"><?php _e( 'Menu', 'enormous' ); ?></h1>
