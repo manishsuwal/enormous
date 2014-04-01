554e406c8e897ebf19de67cfb87fdf897e45c76f
Manish Suwal <enwil410@gmail.com>
2014-04-01 09:44:25 +0545

    Change theme URL

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( 'enormous_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'enormous' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'enormous' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'enormous' ), '<a href="'.esc_url( 'http://enwil.com/enormous' ).'" rel="designer">Enormous</a>', '<a href="'.esc_url('http://enwil.com/').'" rel="designer">Manish Suwal</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'enormous' ), '<a href="'.esc_url( 'http://enwil.com/enormous' ).'" rel="designer">Enormous</a>', '<a href="'.esc_url('http://enwil.com/themes/enormous').'" rel="designer">Manish Suwal</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
