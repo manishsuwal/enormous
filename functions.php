9e629b38495ae9fc7791f494b41d9919ed8ba72c
Manish Suwal <enwil410@gmail.com>
2014-04-02 08:38:54 +0545

    Escape permalink URL at end of functions.php

--- functions.php
+++ functions.php
@@ -146,7 +146,7 @@ add_filter( 'post_thumbnail_html', 'my_post_image_html', 10, 3 );
 
 function my_post_image_html( $html, $post_id, $post_image_id ) {
 
-  $html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
+  $html = '<a href="' . esc_url( get_permalink( $post_id ) ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
   return $html;
 
 }
